b48ea2a28b5716082ec2a724c0937a7b7ea084ab
Kyler <Krenneker16@gmail.com>
2019-11-26 21:17:05 -0500

    added new section

--- src/components/LandProducts/LandProducts.js
+++ src/components/LandProducts/LandProducts.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import img1 from '../../assets/images/featShoe1.jpg'
+import img2 from '../../assets/images/featShoe2.jpg'
+import img3 from '../../assets/images/featShoe3.jpg'
+import styled from 'styled-components'
 
-export default function LandProduct() {
+const FeatureList = styled.ul`
+    display: flex;
+`
+
+export default function LandProducts() {
+
+    const renderListItems = (images) => {
+        let content = [];
+
+        images.map((image, i) => {
+            content.push(
+                <img src={image} />
+            )
+        })
+
+        return content
+    }
+
+
     return (
         <>
+            <h2>
+                Handmade products
+            </h2>
+            <FeatureList>
+                {renderListItems([img1, img2, img3])}
+            </FeatureList>
         </>
     )
 }
